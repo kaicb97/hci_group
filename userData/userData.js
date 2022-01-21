de1599ce02ac068c0e655fa380cf467840057bbb
DFG-Timethie <34272187+DFG-Timethie@users.noreply.github.com>
2022-01-21 01:51:44 +0100

    Bisschen schöner gemacht

--- userData/userData.js
+++ userData/userData.js
@@ -1,39 +1,7 @@
 var numberOfPeople = 3;
 
-var table = `
-<div>
-    <table>
-    <tr>
-        <td>
-            <label for="FirstName">First Name</label>, <label for="LastName">Last Name*:</label>
-        </td>
-        <td>
-            <input name="FirstName" id="FirstName" type="text" maxlength="60" style="max-width:146px;width:100%">
-            <input name="LastName" id="LastName" type="text" maxlength="60" style="max-width:146px;width:100%">
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <label for="FirstName">E-Mail*</label>
-        </td>
-        <td>
-            <input name="FirstName" id="Email" type="text" maxlength="60" style="max-width:300px;width:100%">
-        </td>
-    </tr>
-    </table>
-</div>`;
-
-
-function createInputTables() {
-    
+function init() {
     document.getElementById("preis").innerHTML = "<h1>"+localStorage.getItem("price")+"Euro</h1>";
-    var input= "";
-    for (let index = 0; index < 1; index++) {
-        input += '<div class="formBG"><label><h1>Persoehnliche Daten</h1></label>';
-        input += table;
-    }
-    input += '<div class="submit"><button class="mybutton" onClick="send()">submit</button></div></div>';
-    document.getElementById("finalForm").innerHTML = input;
 }
 
 function send() {
